92a129ebf9f122e87a204c161e0a96ec4978df47
SFYLL <santiagoflood@hotmail.fr>
2023-04-14 16:18:59 +0200

    load via json

---
+++ test_python/encoding_and_hashing/test_beacon_bid_trace.py
@@ -0,0 +1,55 @@
+from eth2spec.phase0.spec import  Slot, Root, Container, BLSPubkey
+from eth2spec.utils.ssz.ssz_typing import ByteVector, uint64, uint256
+
+from test_python.test_lib import serialize_object, get_hash_tree_root_bid_trace, serialize_test, load_json_data
+
+
+"""
+Bid Trace Encoding Test
+"""
+
+class Address(ByteVector[20]):
+    pass
+
+class BidTrace(Container):
+    slot: Slot
+    parent_root: Root
+    state_root: Root
+    BuilderPubkey: BLSPubkey
+    ProposerPubkey: BLSPubkey
+    ProposerFeeRecipient: Address
+    GasLimit: uint64
+    GasUsed: uint64
+    Value: uint256
+
+bid_trace = load_json_data("bid_trace.json")
+bid_trace_ssz_encoded = load_json_data("bid_trace_ssz_encoded.json")
+
+obj = BidTrace(
+    slot=Slot(bid_trace['slot']),
+    parent_root=Root(bid_trace['parentRoot']),
+    state_root=Root(bid_trace['stateRoot']),
+    BuilderPubkey=BLSPubkey(bid_trace['builderPubkey']),
+    ProposerPubkey=BLSPubkey(bid_trace['proposerPubkey']),
+    ProposerFeeRecipient=Address(bid_trace['proposerFeeRecipient']),
+    GasLimit=uint64(bid_trace['gasLimit']),
+    GasUsed=uint64(bid_trace['gasUsed']),
+    Value=uint256(bid_trace['value']),
+)
+
+#Serialize using library
+serialized_obj = serialize_object(obj)
+print(f"{serialized_obj.hex()=}")
+
+#Serialize using custom function
+expected_ssz_encoded = serialize_test([bid_trace_ssz_encoded["slot"], bid_trace_ssz_encoded["parentRoot"], bid_trace_ssz_encoded["stateRoot"], bid_trace_ssz_encoded["builderPubkey"], bid_trace_ssz_encoded["proposerPubkey"], bid_trace_ssz_encoded["proposerFeeRecipient"], bid_trace_ssz_encoded["gasLimit"], bid_trace_ssz_encoded["gasUsed"], bid_trace_ssz_encoded["value"]])
+
+#get hashtreeroot using library
+hashtreeroot = obj.hash_tree_root()
+print(f"{hashtreeroot.hex()=}")
+
+# #get hashtreeroot using custom function
+expected_hashtreeroot = get_hash_tree_root_bid_trace(bid_trace_ssz_encoded["slot"], bid_trace_ssz_encoded["parentRoot"], bid_trace_ssz_encoded["stateRoot"], bid_trace_ssz_encoded["builderPubkey"], bid_trace_ssz_encoded["proposerPubkey"], bid_trace_ssz_encoded["proposerFeeRecipient"], bid_trace_ssz_encoded["gasLimit"], bid_trace_ssz_encoded["gasUsed"], bid_trace_ssz_encoded["value"])
+
+assert(serialized_obj.hex() == expected_ssz_encoded)
+assert(expected_hashtreeroot == hashtreeroot.hex())
---
+++ test_python/encoding_and_hashing/test_beacon_block_header.py
@@ -0,0 +1,31 @@
+from eth2spec.phase0.spec import  BeaconBlockHeader, Slot, ValidatorIndex, Root
+
+from test_python.test_lib import serialize_object, get_hash_tree_root_beacon_block_header, load_json_data
+
+"""
+Beacon Block Header Encoding Test
+"""
+
+beacon_block_header = load_json_data("beacon_block_header.json")
+beacon_block_header_ssz_encoded = load_json_data("beacon_block_header_ssz_encoded.json")
+
+obj = BeaconBlockHeader(
+    slot=Slot(beacon_block_header['slot']),
+    proposer_index=ValidatorIndex(beacon_block_header['proposerIndex']),
+    parent_root=Root(beacon_block_header['parentRoot']),
+    state_root=Root(beacon_block_header['stateRoot']),
+    body_root=Root(beacon_block_header['bodyRoot']),
+)
+
+# Serialize beacon block header using library
+serialized_block_header = serialize_object(obj)
+hashtreeroot = obj.hash_tree_root()
+print(f"{serialized_block_header.hex()=}")
+print(f"{hashtreeroot.hex()=}")
+
+#get hashtreeroot using custom function
+expected_hashtreeroot = get_hash_tree_root_beacon_block_header(beacon_block_header_ssz_encoded["slot"], beacon_block_header_ssz_encoded["proposerIndex"], beacon_block_header_ssz_encoded["parentRoot"], beacon_block_header_ssz_encoded["stateRoot"], beacon_block_header_ssz_encoded["bodyRoot"])
+
+# Checking output with https://www.ssz.dev/chainsafe
+assert("0x" + serialized_block_header.hex() == "0x03000000000000000200000000000000fe1bc5762ac63d5f9d51f9e422746f7f30da42a57dc70567e99b22afd4718622ed099057b8526e16b3d276e12f0756c9363a4ed350f61c667f4be4df89872a1cfa29a0c1d120219c4bcd89e188fbfcfd5e2f7ad9e83bd1b38549896b0d231ba5")
+assert(hashtreeroot.hex() == expected_hashtreeroot)
