36efe78ba3bf05b89a89131e57cdbe470d3fba48
SFYLL <santiagoflood@hotmail.fr>
2023-04-18 10:34:15 +0200

    update test

--- test/Contractor.t.sol
+++ test/Contractor.t.sol
@@ -12,13 +12,14 @@ import {ContractorFixture} from "test/fixture.sol";
 contract ContractorTest is Test, ContractorFixture {
 
     Contractor public contractor;
+
     BeaconBlockHeader public header;
     bytes public beaconBlockHeaderBlsSignature;
+    bytes32 public beaconBlockHeaderHashTreeRoot;
+    
     BidTrace public bidTrace;
     bytes public bidTraceBlsSignature;
-    bytes public beaconBlockHeaderSignature;
-    bytes public bidTraceSignature;
-
+    bytes32 public bidTraceHashTreeRoot;
     
      function setUp() public {
         contractor = new Contractor();
@@ -35,12 +36,15 @@ contract ContractorTest is Test, ContractorFixture {
         function loadBeaconBlockHeaderAndSig(string memory root) public {
             string memory beaconBlockHeaderContent = vm.readFile(string.concat(root, "/test_data/beacon_block_header.json"));
             string memory beaconBlockHeaderBlsSig = vm.readFile(string.concat(root, "/test_data/beacon_block_header_bls_sig.json"));
+            string memory beaconBlockHeaderHashTreeRootContent = vm.readFile(string.concat(root, "/test_data/beacon_block_header_hash_tree_root.json"));
 
             bytes memory beaconBlockHeaderData = vm.parseJson(beaconBlockHeaderContent);
             bytes memory beaconBlockHeaderBlsSigData = vm.parseJson(beaconBlockHeaderBlsSig);
+            bytes memory beaconBlockHeaderHashTreeRootData = vm.parseJson(beaconBlockHeaderHashTreeRootContent);
 
             BeaconBlockHeaderFixture memory beaconBlockHeaderFixture = abi.decode(beaconBlockHeaderData, (BeaconBlockHeaderFixture));
             beaconBlockHeaderBlsSignature = abi.decode(beaconBlockHeaderBlsSigData, (bytes));
+            beaconBlockHeaderHashTreeRoot = abi.decode(beaconBlockHeaderHashTreeRootData, (bytes32));
 
             header = newBeaconBlockHeader(beaconBlockHeaderFixture);
         }
@@ -48,46 +52,28 @@ contract ContractorTest is Test, ContractorFixture {
         function loadBeaconBidTraceAndSig(string memory root) public {
             string memory bidTraceContent = vm.readFile(string.concat(root, "/test_data/bid_trace.json"));
             string memory bidTraceSig = vm.readFile(string.concat(root, "/test_data/bid_trace_bls_sig.json"));
+            string memory bidTraceHashTreeRootContent = vm.readFile(string.concat(root, "/test_data/bid_trace_hash_tree_root.json"));
 
             bytes memory bidTraceData = vm.parseJson(bidTraceContent);
             bytes memory bidTraceBlsSigData = vm.parseJson(bidTraceSig);
-
-            console.logBytes(bidTraceData);
+            bytes memory bidTraceHashTreeRootData = vm.parseJson(bidTraceHashTreeRootContent);
 
             BidTraceFixture memory bidTraceFixture = abi.decode(bidTraceData, (BidTraceFixture));
-
-            console.logBytes(bidTraceFixture.builderPubkey);
             bidTraceBlsSignature = abi.decode(bidTraceBlsSigData, (bytes));
+            bidTraceHashTreeRoot = abi.decode(bidTraceHashTreeRootData, (bytes32));
 
             bidTrace = newBidTrace(bidTraceFixture);
         }
 
     function test_getHashTreeRootBlockHeader() public view{
-        bytes32 expectedHashTreeRoot = 0xece514576a14314b37af4d185d905f31c54b47822dec10ff7e78641dd6a7f9d2;
         bytes32 hashTreeRoot = contractor.getHashTreeRootBlockHeader(header);
 
-        assert(hashTreeRoot == expectedHashTreeRoot);
+        assert(hashTreeRoot == beaconBlockHeaderHashTreeRoot);
     }
-
-    function test_getEncodedBlockHeader() public view{
-        bytes memory expectedEncodedBlockHeader = hex"03000000000000000200000000000000fe1bc5762ac63d5f9d51f9e422746f7f30da42a57dc70567e99b22afd4718622ed099057b8526e16b3d276e12f0756c9363a4ed350f61c667f4be4df89872a1cfa29a0c1d120219c4bcd89e188fbfcfd5e2f7ad9e83bd1b38549896b0d231ba5";
-        bytes memory encodedBlockHeader = contractor.getEncodedBlockHeader(header);
-
-        assert(keccak256(encodedBlockHeader) == keccak256(expectedEncodedBlockHeader));
-    }
-
-    function test_getEncodedBidTrace() public view {
-        bytes memory encodedBidTrace = contractor.getEncodedBidTrace(bidTrace);
-        bytes memory expectedEncodedBidTrace = hex"0100000000000000cf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2cf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f293247f2209abcacf57b75a51dafae777f9dd38bc7053d1af526f220a7489a6d3a2753e5f3e8b1cfe39b56f43611df74a93247f2209abcacf57b75a51dafae777f9dd38bc7053d1af526f220a7489a6d3a2753e5f3e8b1cfe39b56f43611df74aabcf8e0d4e9587369b2301d0790347320302cc090100000000000000010000000000000000000040eaed7446d09c2c9f0c00000000000000000000000000000000000000";
-        
-        assert(keccak256(encodedBidTrace) == keccak256(expectedEncodedBidTrace));
-    }
-
 
     function test_getHashTreeRootBidTrace() public view {
         bytes32 hashTreeRoot = contractor.getHashTreeRootBidTrace(bidTrace);
-        bytes32 expectedHashTreeRoot = 0x3272b07b7bf5b240b0da5835b1a6350f73010a49eb94cdb4a44ff392d8303473;
 
-        assert(hashTreeRoot == expectedHashTreeRoot);
+        assert(hashTreeRoot == bidTraceHashTreeRoot);
     }
 }
