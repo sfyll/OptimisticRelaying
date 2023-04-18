bea363c5fcb5e0e1bd5d14ff5bbd631c5f54f2ed
SFYLL <santiagoflood@hotmail.fr>
2023-04-18 09:24:09 +0200

    reformat builder proposer

---
+++ test_python/signing_and_verifying/test_verifier.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+from eth2spec.phase0.spec import compute_domain, compute_signing_root
+
+domain = Tuple[bytes, ...]
+domainType = Tuple[bytes, ...]
+forkVersion = Tuple[bytes, ...]
+
+domainTypeBeaconProposer = b'\x00' + b'\x00' + b'\x00' + b'\x00'
+
+def get_proposer_domain(domainTypeBeaconProposer, forkVersion = None, root = None) -> bytes:
+    return compute_domain(domainTypeBeaconProposer, forkVersion, root)
+
+proposer_domain = get_proposer_domain(domainTypeBeaconProposer)
+
+#check that returned domain is 32 bytes long
+assert(len(proposer_domain) == 32 and isinstance(proposer_domain, bytes))
