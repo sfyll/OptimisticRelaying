ddc4ef9ff3b2c2851b90a304b864044fc4f0ca1b
SFYLL <santiagoflood@hotmail.fr>
2023-04-20 16:06:42 +0200

    implementing most of the logic

--- src/Contractor.sol
+++ src/Contractor.sol
@@ -1,94 +1,67 @@
-pragma solidity ^0.8.16;
+pragma solidity >=0.8.6 <0.9.0;
 
-import {SSZ, BeaconBlockHeader} from "telepathy-contracts/src/libraries/SimpleSerialize.sol";
-import {BytesLib} from "telepathy-contracts/src/libraries/MessageEncoding.sol";
+import {AccountHandler} from"./lib/accountHandler.sol";
+import {BeaconBlockHeader, BidTrace, SSZUtilities} from"./lib/SSZUtilities.sol";
 
-struct BidTrace {
-    uint64 slot;
-    bytes32 parentRoot;
-    bytes32 stateRoot;
-    bytes builderPubkey;
-    bytes proposerPubkey;
-    address proposerFeeRecipient;
-    uint64 gasLimit;
-    uint64 gasUsed;
-    uint256 value;
+struct Groth16Proof {
+    uint256[2] a;
+    uint256[2][2] b;
+    uint256[2] c;
 }
 
-contract Contractor {
+contract Contractor is AccountHandler, SSZUtilities{
 
-    function getSigningRootBeaconBlockHeader(BeaconBlockHeader memory header, bytes32 domain) public pure returns (bytes32) {
-        return sha256(
-            bytes.concat(
-                getHashTreeRootBlockHeader(header),
-                domain
-            )
-        );
+    //@verify existance of BLS address in array
+    //@param index of array where hash of bls address is stored
+    function verifyBlsAddressWasCommited(bytes memory blsPubKey, address eth1Address) internal view returns (bool) {
+        bytes32 hashedBlsPubKey = sha256(abi.encodePacked(blsPubKey));
+        BuilderMetaDatas storage buildermetaData = builderMetaDatas[eth1Address];
+
+         for (uint256 i = 0; i < buildermetaData.hashCommitedBlsAddress.length; i++) {
+            if (hashedBlsPubKey == buildermetaData.hashCommitedBlsAddress[i]) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    function getSigningRootBidTrace(BidTrace memory bidTrace, bytes32 domain) public pure returns (bytes32) {
-        return sha256(
-            bytes.concat(
-                getHashTreeRootBidTrace(bidTrace),
-                domain
-            )
-        );
+    //@notice get hash of a eth1 address blspubkey
+    //@param eth1 address and array index up to which we want to hash
+    function getHashCommitedBlsAddress(bytes[] memory blsPubKey) public pure returns (bytes32[] memory) {
+        bytes32[] memory hashCommitedBlsAddress = new bytes32[](blsPubKey.length);
+
+        for (uint256 i = 0; i < blsPubKey.length; i++) {
+            hashCommitedBlsAddress[i] = sha256(blsPubKey[i]);
+        }
+        return hashCommitedBlsAddress;    
     }
 
-    function getHashTreeRootBlockHeader(BeaconBlockHeader memory header) public pure returns (bytes32) {
-        return SSZ.sszBeaconBlockHeader(header);
-    }
-
-    function getHashTreeRootBidTrace(BidTrace memory bidTrace) public pure returns (bytes32) {
-        bytes32 builderPubkeyChunk = sha256(
-            bytes.concat(
-            BytesLib.slice(bidTrace.builderPubkey, 0, 32),
-            bytes.concat(BytesLib.slice(bidTrace.builderPubkey, 32, 16), bytes16(0))
-            )
-        );
-        bytes32 proposerPubkeyChunk = sha256(
-            bytes.concat(
-            BytesLib.slice(bidTrace.proposerPubkey, 0, 32),
-            bytes.concat(BytesLib.slice(bidTrace.proposerPubkey, 32, 16), bytes16(0))
-            )
+    //@notice submit builder invalid block + zk Groth16Proof
+    //@dev this function is called by the relayer and allow to verify
+    //both builder and proposer commited on the same block.
+    function invalidBlockDispute(BeaconBlockHeader memory header, bytes32 proposerDomain,
+                                 BidTrace memory bidTrace, bytes32 builderDomain,
+                                 address builderPubKeyEth1, Groth16Proof memory proof)
+                                 external payable {
+        require(builderMetaDatas[builderPubKeyEth1].exists == true, "Builder doesn't exists");
+        require(builderMetaDatas[builderPubKeyEth1].balance >= bidTrace.value, "Insufficient balance");
+        require(verifyBlsAddressWasCommited(bidTrace.builderPubkey, builderPubKeyEth1), "BLS address doesn't exist");
+        require(
+            header.slot == bidTrace.slot &&
+            header.parentRoot == bidTrace.parentRoot &&
+            header.stateRoot == bidTrace.stateRoot
         );
 
-        bytes memory rightPaddedProposerFeeRecipientAddress = bytes.concat(abi.encodePacked(bidTrace.proposerFeeRecipient), hex"000000000000000000000000");
+        bytes32 bidTraceSigningRoot = getSigningRootBidTrace(bidTrace, builderDomain);
+        bytes32 headerSigningRoot = getSigningRootBeaconBlockHeader(header, proposerDomain);
 
-        bytes32 left = sha256(
-            bytes.concat(
-                sha256(
-                    bytes.concat(
-                        sha256(bytes.concat(SSZ.toLittleEndian(bidTrace.slot), bidTrace.parentRoot)),
-                        sha256(bytes.concat(bidTrace.stateRoot, builderPubkeyChunk))
-                    )
-                ),
-                sha256(
-                    bytes.concat(
-                        sha256(bytes.concat(proposerPubkeyChunk, rightPaddedProposerFeeRecipientAddress)),
-                        sha256(bytes.concat(SSZ.toLittleEndian(bidTrace.gasLimit), SSZ.toLittleEndian(bidTrace.gasUsed)))
-                    )
-                )
-            )
-        );
-        
-        bytes32 right = sha256(
-            bytes.concat(
-                sha256(
-                    bytes.concat(
-                        sha256(bytes.concat(SSZ.toLittleEndian(bidTrace.value), bytes32(0))),
-                        sha256(bytes.concat(bytes32(0), bytes32(0)))
-                    )
-                ),
-                sha256(
-                    bytes.concat(
-                        sha256(bytes.concat(bytes32(0), bytes32(0))),
-                        sha256(bytes.concat(bytes32(0), bytes32(0)))
-                    )
-                )
-            )
-        );
+        // Below will be implemented via snark-js automatically. What won't be are the circuits!
+        // Nonetheless, since all inputs are fixed size SSZ encoded is trivial and one can
+        // rely mostly on what was built by succinctlabs and 0xParc!
+        // require(verifySignature(proof.a, proof.b, proof.c, bidTraceSigningRoot), "Invalid signature");
+        // require(verifySignature(proof.a, proof.b, proof.c, headerSigningRoot), "Invalid signature");
 
-        return sha256(bytes.concat(left, right));
+        builderMetaDatas[msg.sender].balance -= bidTrace.value;
+        bidTrace.proposerFeeRecipient.transfer(bidTrace.value);
     }
 }
