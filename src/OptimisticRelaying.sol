6dc1de39f543272f99e4027b25279dd47ba67996
SFYLL <santiagoflood@hotmail.fr>
2023-04-21 16:37:27 +0200

    rename

--- src/OptimisticRelaying.sol
+++ src/OptimisticRelaying.sol
@@ -9,11 +9,19 @@ struct Groth16Proof {
     uint256[2] c;
 }
 
-contract Contractor is AccountHandler, SSZUtilities{
+contract OptimisticRelaying is AccountHandler, SSZUtilities {
 
-    //@verify existance of BLS address in array
-    //@param index of array where hash of bls address is stored
-    function verifyBlsAddressWasCommited(bytes memory blsPubKey, address eth1Address) internal view returns (bool) {
+    uint64 public immutable FIRST_VALID_SLOT;
+
+    ///@notice enforce a first valid slot that should ideally be the one at deploy time
+    ///@param firstValidSlot is ideally the curent slot at deploy time
+    ///@dev this allows to set a lower bound in which block can be disputed in case of mallicious behavior
+    constructor(uint64 firstValidSlot) {
+        FIRST_VALID_SLOT = firstValidSlot;
+    }
+
+    ///@notice verify existance of BLS address in array
+    function verifyBlsAddressWasCommited(bytes memory blsPubKey, address eth1Address) public view returns (bool) {
         bytes32 hashedBlsPubKey = sha256(abi.encodePacked(blsPubKey));
         BuilderMetaDatas storage buildermetaData = builderMetaDatas[eth1Address];
 
@@ -25,8 +33,7 @@ contract Contractor is AccountHandler, SSZUtilities{
         return false;
     }
 
-    //@notice get hash of a eth1 address blspubkey
-    //@param eth1 address and array index up to which we want to hash
+    ///@notice get hash of a eth1 address blspubkey
     function getHashCommitedBlsAddress(bytes[] memory blsPubKey) public pure returns (bytes32[] memory) {
         bytes32[] memory hashCommitedBlsAddress = new bytes32[](blsPubKey.length);
 
@@ -43,14 +50,15 @@ contract Contractor is AccountHandler, SSZUtilities{
                                  BidTrace memory bidTrace, bytes32 builderDomain,
                                  address builderPubKeyEth1, Groth16Proof memory proof)
                                  external payable {
+        require(header.slot >= FIRST_VALID_SLOT, "Given slot is to old");
         require(builderMetaDatas[builderPubKeyEth1].exists == true, "Builder doesn't exists");
         require(builderMetaDatas[builderPubKeyEth1].balance >= bidTrace.value, "Insufficient balance");
         require(verifyBlsAddressWasCommited(bidTrace.builderPubkey, builderPubKeyEth1), "BLS address doesn't exist");
         require(
-            header.slot == bidTrace.slot &&
+            (header.slot == bidTrace.slot &&
             header.parentRoot == bidTrace.parentRoot &&
             header.stateRoot == bidTrace.stateRoot
-        );
+        ), "block content does not match");
 
         bytes32 bidTraceSigningRoot = getSigningRootBidTrace(bidTrace, builderDomain);
         bytes32 headerSigningRoot = getSigningRootBeaconBlockHeader(header, proposerDomain);
