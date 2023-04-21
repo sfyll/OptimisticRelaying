9480a66b04da5a99d309ae7556813ce32f30f152
SFYLL <santiagoflood@hotmail.fr>
2023-04-21 12:21:50 +0200

    fullTest accHandler

--- test/accountHandler.t.sol
+++ test/accountHandler.t.sol
@@ -9,79 +9,55 @@ import {Fixture} from "test/fixture.sol";
 contract AccountHandlerTest is Fixture {
     AccountHandler accountHandler;
 
-    //@dev: we use any bytes array as a fake bls address since
-    //we manipulate their sha256 hash which is of size 32 bytes
-    function testDeposit(uint256 value, bytes[] memory fakeBlsAddy) public {
+    function depositAndGetHashOfCommitedBlsAddress(uint256 value, bytes[] memory fakeBlsAddy) public returns (uint256, bytes32[] memory) {
         value = accountHandlerFuzzingParamsDeposit(value, fakeBlsAddy);
-        accountHandler = newAccountHandler();
         
         bytes32[] memory hashCommitedBlsAddress = getHashCommitedBlsAddress(fakeBlsAddy);
         
         accountHandler.deposit{value: value}(hashCommitedBlsAddress);
 
+        return (value, hashCommitedBlsAddress);
+    }
+
+    //@dev: we use any bytes array as a fake bls address since
+    //we manipulate their sha256 hash which is of size 32 bytes
+    function testDeposit(uint256 value, bytes[] memory fakeBlsAddy) public {
+        accountHandler = newAccountHandler();
+
+        (uint256 value, bytes32[] memory hashCommitedBlsAddress) = depositAndGetHashOfCommitedBlsAddress(value, fakeBlsAddy);
+
         uint256 balance = address(accountHandler).balance;
         bytes32[] memory inContractHashedBlsAdd = accountHandler.getBuilderHashCommitedBlsAddress(address(this));
 
-        assertEq(balance, value, "The contract balance should be 1 ether after deposit");
+        assertEq(balance, value, "The contract balance should be deposit ether after deposit");
         assertEq(keccak256(abi.encodePacked(inContractHashedBlsAdd)), keccak256(abi.encodePacked(hashCommitedBlsAddress)), "Contract Commited Address should be the one we commited");
     }
-
-    // function testAddSimple() public {
-    //     accountHandler = newAccountHandler();
-    //     uint256 deposit = 1 ether;
-    //     uint256 addedCollateral = 1 ether;
-
-    //     vm.deal(address(this), 100 ether);
-
-    //     bytes32[] memory hashCommitedBlsAddress = new bytes32[](1);
-    //     hashCommitedBlsAddress[0] = sha256(abi.encodePacked("fake 1"));
-
-    //     accountHandler.deposit{value: deposit}(hashCommitedBlsAddress);
-        
-    //     bytes32[] memory hashCommitedBlsAddress2 = new bytes32[](1);
-    //     hashCommitedBlsAddress2[0] = sha256(abi.encodePacked("fake 2"));
-        
-    //     accountHandler.add{value: addedCollateral}(hashCommitedBlsAddress2);
-
-    //     uint256 balance = address(accountHandler).balance;
-    //     bytes32[] memory inContractHashedBlsAdd = accountHandler.getBuilderHashCommitedBlsAddress(address(this));
-    //     bytes32[] memory commitedHashes = new bytes32[](2);
-    //     commitedHashes[0] = hashCommitedBlsAddress[0];
-    //     commitedHashes[1] = hashCommitedBlsAddress2[0];
-
-    //     assertEq(balance, deposit + addedCollateral, "The contract balance should be 1 ether after deposit");
-    //     assertEq(keccak256(abi.encodePacked(inContractHashedBlsAdd)), keccak256(abi.encodePacked(commitedHashes)), "Contract Commited Address should be the one we commited");    
-    // }
-
 
     function testAdd(uint256 deposit, uint256 addedCollateral, bytes[] memory fakeBlsAddyDeposit,
                      bytes[] memory fakeBlsAddyAdd) public {
         accountHandler = newAccountHandler();
-        (deposit, addedCollateral) = accountHandlerFuzzingParamsAdd(deposit, addedCollateral, fakeBlsAddyDeposit, fakeBlsAddyAdd);
-        console.log(fakeBlsAddyDeposit.length);
-        console.log(fakeBlsAddyAdd.length);
 
-        bytes32[] memory hashCommitedBlsAddressDeposit = getHashCommitedBlsAddress(fakeBlsAddyDeposit);
+        (uint256 deposit, bytes32[] memory hashCommitedBlsAddressDeposit) = depositAndGetHashOfCommitedBlsAddress(deposit, fakeBlsAddyDeposit);
+
+        addedCollateral = accountHandlerFuzzingParamsAdd(addedCollateral, fakeBlsAddyAdd);
+
         bytes32[] memory hashCommitedBlsAddressAdd = getHashCommitedBlsAddress(fakeBlsAddyAdd);
-
-        accountHandler.deposit{value: deposit}(hashCommitedBlsAddressDeposit);
-
-        console.log("deposit second half");
 
         accountHandler.add{value: addedCollateral}(hashCommitedBlsAddressAdd);
 
         uint256 balance = address(accountHandler).balance;
+
         bytes32[] memory inContractHashedBlsAdd = accountHandler.getBuilderHashCommitedBlsAddress(address(this));
-        console.log("GOT");
         bytes32[] memory ourCommittedHashedBlsAddies = aggregateBlsAddressHashes(hashCommitedBlsAddressDeposit, hashCommitedBlsAddressAdd);
 
-        assertEq(balance, deposit + addedCollateral, "The contract balance should be 1 ether after deposit");
+        assertEq(balance, deposit + addedCollateral, "The contract balance should be deposit + addedCollateral ether after deposit");
         assertEq(keccak256(abi.encodePacked(inContractHashedBlsAdd)), keccak256(abi.encodePacked(ourCommittedHashedBlsAddies)), "Contract Commited Address should be the one we commited");    
     }
 
-    function testInstantiateTransfer(uint256 value) public {
-        bytes32[] memory hashCommitedBlsAddress = new bytes32[](0);
-        accountHandler.deposit{value: value}(hashCommitedBlsAddress);
+    function testInstantiateTransfer(uint256 value, bytes[] memory fakeBlsAddy) public {
+        accountHandler = newAccountHandler();
+        
+        (uint256 value, bytes32[] memory __) = depositAndGetHashOfCommitedBlsAddress(value, fakeBlsAddy);
 
         accountHandler.instantiateTransfer();
         uint256 releaseTime = accountHandler.getBuilderReleaseTime(address(this));
@@ -89,16 +65,16 @@ contract AccountHandlerTest is Fixture {
         assertTrue(releaseTime > 0, "The release time should be set after initiateTransfer");
     }
 
-    function testWithdraw(uint256 value) public {
-        vm.deal(address(this), 1 ether);
-        bytes32[] memory hashCommitedBlsAddress = new bytes32[](0);
-        accountHandler.deposit{value: value}(hashCommitedBlsAddress);
+    function testWithdraw(uint256 value, bytes[] memory fakeBlsAddy) public {
+        accountHandler = newAccountHandler();
+
+        (uint256 value, bytes32[] memory hashCommitedBlsAddress) = depositAndGetHashOfCommitedBlsAddress(value, fakeBlsAddy);
 
         accountHandler.instantiateTransfer();
         uint256 releaseTime = accountHandler.getBuilderReleaseTime(address(this));
 
         uint256 currentTimestamp = block.timestamp;
-        vm.warp(7 days);
+        vm.warp(8 days);
         uint256 newTimestamp = block.timestamp;
 
         assertTrue(newTimestamp > releaseTime, "The new timestamp should be greater than the release time");
